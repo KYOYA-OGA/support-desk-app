2943e41a9428699f457a4bbb701334ccdfeb9e37
KYOYA OGASAWARA <64949271+KYOYA-OGA@users.noreply.github.com>
2022-03-19 15:32:43 +0700

    fixed return isuues

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,13 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html');
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
 } else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk App' });
-  });
+  app.get('/', (req, res) =>
+    res.status(200).json({ message: 'Welcome to the Support Desk App' })
+  );
 }
 
 app.use(errorHandler);
